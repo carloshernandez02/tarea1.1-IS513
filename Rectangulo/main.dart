db6df830a8b024b871e3cb465088f870a5039aec
carloshernandez02 <cahernandezn@unah.hn>
2024-10-08 13:51:13 -0600

    :D

--- Rectangulo/main.dart
+++ Rectangulo/main.dart
@@ -1,6 +1,9 @@
 import 'Rectangulo.dart';
 import 'dart:math';
 void main(){
-  Rectangulo sqr = Rectangulo(ancho: Random().nextInt(25).ceilToDouble()+Random().nextDouble(), largo: Random().nextInt(25).ceilToDouble()+Random().nextDouble());
+  Rectangulo sqr = Rectangulo(ancho: Random().nextInt(25).ceilToDouble()+Random().nextDouble(), 
+  largo: Random().nextInt(25).ceilToDouble()+Random().nextDouble()); 
+  // La instancia se contruye con ancho y largo aleatorios y se le suma un decimal aleatorio
+  
   print('El area del rectangulo de ${sqr.ancho} de ancho y ${sqr.largo} de largo es de ${sqr.Area()} cm');
 }
