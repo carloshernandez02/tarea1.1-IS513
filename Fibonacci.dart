fc864a570fba9a6605da81925b02d0ea58792cbb
carloshernandez02 <cahernandezn@unah.hn>
2024-10-07 22:41:44 -0600

    x

---
+++ Fibonacci.dart
@@ -0,0 +1,8 @@
+void main(){
+  print("Fibonacci: ");
+  List <int> fibonacci = [0,1];
+  for(int i=1;i<14;i++){
+    fibonacci.add(fibonacci[i-1]+fibonacci[i]);
+  }
+  print("$fibonacci");
+}
---
+++ SumaLista.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+void main(){
+  List<int> lista=List.empty(growable: true);
+  for(int i=1;i<Random().nextInt(16)+2; i++)
+    lista.add(Random().nextInt(70)); 
+ 
+  print("Lista: $lista");
+  int suma=0;
+  
+  for(int i=0;i<lista.length;i++)
+    suma+=lista[i];
+  print("Suma de lista es: $suma");
+  
+}
